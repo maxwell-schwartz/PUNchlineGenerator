2d87e27ffcf54253ec42c12b1c9d4763faafb409
max <maxwell.schwartz11@gmail.com>
2017-09-05 20:26:36 -0400

    Setup scripts added

---
+++ other_requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source activate pun_generator
+pip install metaphone fuzzywuzzy
+pip install fuzzywuzzy[speedup]
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+conda create --name pun_generator --file conda_requirements.txt -y
+source other_requirements.sh
